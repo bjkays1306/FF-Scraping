87e38bd4e97de5cb58ba0528119a4c2353b57548
bjkays1306 <benjaminjkayser@gmail.com>
2025-08-22 19:43:10 -0400

    Update cookieString.py

--- cookieString.py
+++ cookieString.py
@@ -1,6 +1,13 @@
 
 # Paste your cookie string here
-cookie_string = ""
+cookie_string = "curl ^"https://www.nfl.com/compiledassets/css/base.css?_t=a4565cfda331459519de131b259fb20e^" ^
+  -H ^"sec-ch-ua-platform: ^\^"Windows^\^"^" ^
+  -H ^"If-None-Match: ^\^"1dc0e1e2d83dbae^\^"^" ^
+  -H ^"Referer: https://fantasy.nfl.com/^" ^
+  -H ^"User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36^" ^
+  -H ^"sec-ch-ua: ^\^"Not;A=Brand^\^";v=^\^"99^\^", ^\^"Google Chrome^\^";v=^\^"139^\^", ^\^"Chromium^\^";v=^\^"139^\^"^" ^
+  -H ^"If-Modified-Since: Fri, 15 Aug 2025 19:52:49 GMT^" ^
+  -H ^"sec-ch-ua-mobile: ?0^""
 
 # Dict to store cookies
 cookies = {}
